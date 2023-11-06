e47038549e0beac9e1a6039935628d1f9d5d245b
kristian <mlk299@pm.me>
2023-11-06 20:00:42 +0100

    bug fix get_signature

--- handout/src/networking.c
+++ handout/src/networking.c
@@ -84,11 +84,25 @@ void get_signature(char* password, char* salt, hashdata_t* hash)
     // to add more, or work in other parts of the code
     
     // create salted password string
-    char salted[strlen(password) + strlen(salt)];
-    strcpy(salted, strcat(password, salt));
+    char salted[PASSWORD_LEN + SALT_LEN];
+    memcpy(&salted, password, PASSWORD_LEN);
+    memcpy(&salted[PASSWORD_LEN], salt, SALT_LEN);
     
     // hash salted to make signature
     get_data_sha(salted, *(hash), sizeof(salted), SHA256_HASH_SIZE);
+
+    // Debugging
+    // printf("Salted: ");
+    // for (int i = 0; i < PASSWORD_LEN + SALT_LEN; i++) {
+    //   printf("%c", salted[i]);
+    // }
+    // printf("\n");
+
+    // printf("Hash: ");
+    // for (int i = 0; i < PASSWORD_LEN + SALT_LEN; i++) {
+    //   printf("%x", hash[i]);
+    // }
+    // printf("\n");
 }
 
 /*
@@ -102,23 +116,23 @@ void register_user(char* username, char* password, char* salt)
 
     // struct for pass and salt
     PasswordAndSalt_t ps;
-    strncpy(ps.password, password, PASSWORD_LEN);
-    strncpy(ps.salt, salt, SALT_LEN);
+    memcpy(&ps.password, password, PASSWORD_LEN);
+    memcpy(&ps.salt, salt, SALT_LEN);
 
     // Request Header structxs
     RequestHeader_t request_header;
-    strncpy(request_header.username, username, USERNAME_LEN);
-    request_header.length = htonl(0); // length of requested data, the payload. Set to 0 when registrering user.
+    memcpy(&request_header.username, username, USERNAME_LEN);
+    request_header.length = (uint32_t) htonl(0); // length of requested data, the payload. Set to 0 when registrering user.
 
     // Hashing pass and salt
     get_signature(password, salt, &request_header.salted_and_hashed);
 
     // Print debugging for structs
-    /* printf("Password: %s\n", ps.password);
-    printf("Salt: %s\n", ps.salt);
-    printf("Username: %s\n", request_header.username);
-    printf("Salted and hashed: %s\n", request_header.salted_and_hashed);
-    printf("Length: %u\n", request_header.length); */
+    // printf("Password: %s\n", ps.password);
+    // printf("Salt: %s\n", ps.salt);
+    // printf("Username: %s\n", request_header.username);
+    // printf("Salted and hashed: %u\n", request_header.salted_and_hashed);
+    // printf("Length: %u\n", request_header.length);
     
 
     // Connection variables
@@ -152,8 +166,7 @@ void register_user(char* username, char* password, char* salt)
     compsys_helper_readn(clientfd, readbuf, MAXLINE);
     printf("Got response: %s\n", &readbuf[80]);
 
-    // --> TRY IMPLEMENTATION WITH COMPSYS_HELPER_STATE_T STRUCT!
-    //printf("Got response: %s\n", &state.compsys_helper_buf[80]);
+    close(clientfd);
 }
 
 /*
@@ -166,19 +179,51 @@ void get_file(char* username, char* password, char* salt, char* to_get)
     // Your code here. This function has been added as a guide, but feel free 
     // to add more, or work in other parts of the code
 
-    printf("Type 'get' to retrieve small file, or 'quit' to quit:\n");
+    // struct for pass and salt
+    PasswordAndSalt_t ps;
+    strncpy(ps.password, password, PASSWORD_LEN);
+    strncpy(ps.salt, salt, SALT_LEN);
 
+    // Request struct for requesting file from server
+    Request_t request;
+    memcpy(&request.header.username, username, USERNAME_LEN);
+    request.header.length = (uint32_t) htonl(strlen(to_get));
+    get_signature(password, salt, &request.header.salted_and_hashed); // Hashing pass and salt
+    memcpy(&request.payload, to_get, PATH_LEN);
+
+    // Print debugging for structs
+    printf("Password: %s\n", ps.password);
+    printf("Salt: %s\n", ps.salt);
+    printf("Username: %s\n", request.header.username);
+    printf("Salted and hashed: %u\n", request.header.salted_and_hashed);
+    printf("Length: %u\n", ntohl(request.header.length));
+    printf("Payload: %s\n", request.payload);
+
+    // Open clientfd connection
+    int clientfd;
+    char* host = &server_ip[0];
+    char* port = &server_port[0];
+    if ((clientfd = compsys_helper_open_clientfd(host, port)) < 0) {
+      printf("Socket connection failed with error %i\n", clientfd);
+      exit(EXIT_FAILURE);
+    };
+
+    // Copy request to buffer
     char buf[MAXLINE];
+    memcpy(&buf, (void*) &request, sizeof(request));
 
-    while(fgets(buf, MAXLINE, stdin) != NULL) {
-      if (strncmp(buf, "quit", strlen("quit")) == 0) {
-        exit(EXIT_SUCCESS);
-      }
-      if (strncmp(buf, "get", strlen("get")) == 0) {
-        // retrive small file
-        printf("Cannot retrive small file\n");
-      }
-    }
+    // Send request to server
+    compsys_helper_writen(clientfd, buf, MAXLINE);
+
+    // Read response from server and send to stdout
+    char readbuf[MAXBUF];
+    compsys_helper_readn(clientfd, readbuf, MAXLINE);
+    uint32_t lrdata = (uint32_t) readbuf[0];
+    lrdata = ntohl(lrdata);
+    printf("Length of response data: %u\n", lrdata);
+    printf("Got response: %s\n", &readbuf[80]);
+
+    close(clientfd);
 }
 
 int main(int argc, char **argv)
@@ -264,9 +309,9 @@ int main(int argc, char **argv)
         user_salt[i] = 'a' + (rand() % 26); 
     }
     user_salt[SALT_LEN] = '\0';
-    // strncpy(user_salt, 
-    //    "0123456789012345678901234567890123456789012345678901234567890123\0", 
-    //    SALT_LEN+1);
+    strncpy(user_salt, 
+       "0123456789012345678901234567890123456789012345678901234567890123\0", 
+       SALT_LEN+1);
 
     fprintf(stdout, "Using salt: %s\n", user_salt);
 
@@ -281,15 +326,28 @@ int main(int argc, char **argv)
     // added
     register_user(username, password, user_salt);
 
-    // Retrieve the smaller file, that doesn't not require support for blocks. 
-    // As handed out, this line will run every time this client starts, and so 
-    // should be removed if user interaction is added
-    get_file(username, password, user_salt, "tiny.txt");
+    // User interaction
+    printf("Type 'get' to retrieve small file, or 'quit' to quit:\n");
+    char buf[MAXBUF];
+    while(fgets(buf, MAXLINE, stdin) != NULL) {
+      if (strncmp(buf, "quit", strlen("quit")) == 0) {
+        exit(EXIT_SUCCESS);
+      }
+      if (strncmp(buf, "get", strlen("get")) == 0) {
+        // retrive small file
+        //printf("Cannot retrive small file\n");
+
+        // Retrieve the smaller file, that doesn't not require support for blocks. 
+        // As handed out, this line will run every time this client starts, and so 
+        // should be removed if user interaction is added
+        get_file(username, password, user_salt, "tiny.txt");
+      }
+    }
 
     // Retrieve the larger file, that requires support for blocked messages. As
     // handed out, this line will run every time this client starts, and so 
     // should be removed if user interaction is added
-    get_file(username, password, user_salt, "hamlet.txt");
+    //get_file(username, password, user_salt, "hamlet.txt");
 
     exit(EXIT_SUCCESS);
 }
