9581aefd3ed2fc94604798e36ee8994abbccc3df
kristian <mlk299@pm.me>
2023-12-06 14:55:52 +0100

    get_opcode

--- A5/src/simulate.c
+++ A5/src/simulate.c
@@ -2,7 +2,44 @@
 #include "assembly.h"
 #include <stdio.h>
 
-#define WORD_SIZE  32
+#define REGISTER_LEN  32
+#define OPCODE_LEN    7
+#define RD_LEN        5
+#define FUNCT3_LEN    3
+#define RS1_LEN       4
+#define RS2_LEN       5
+#define FUNCT7_LEN    7
+
+
+unsigned get_opcode(unsigned word) {
+  unsigned opcode;
+  opcode = word << (REGISTER_LEN - OPCODE_LEN);
+  opcode = opcode >> (REGISTER_LEN - OPCODE_LEN);
+
+  return opcode;
+};
+
+unsigned get_funct3(unsigned word) {
+  unsigned funct3;
+  funct3 = word >> OPCODE_LEN + RD_LEN;
+
+  return funct3;
+}
+
+
+
+void funct3(unsigned word) {
+
+  unsigned funct3;
+  funct3 
+  funct3 = word >> OPCODE_LEN + RD_LEN;
+  funct3 = funct3 << OPCODE_LEN + RD_LEN + 
+
+
+  switch 
+}
+
+
 
 long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file) {
 
