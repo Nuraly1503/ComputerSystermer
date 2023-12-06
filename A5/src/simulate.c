ee170d186206d09530f9ff00780a20463936f745
kristian <mlk299@pm.me>
2023-12-06 15:58:02 +0100

    helper functions to decode instructions

--- A5/src/simulate.c
+++ A5/src/simulate.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #define REGISTER_LEN  32
+#define INSTR_LEN     32
 #define OPCODE_LEN    7
 #define RD_LEN        5
 #define FUNCT3_LEN    3
@@ -13,30 +14,42 @@
 
 unsigned get_opcode(unsigned word) {
   unsigned opcode;
-  opcode = word << (REGISTER_LEN - OPCODE_LEN);
-  opcode = opcode >> (REGISTER_LEN - OPCODE_LEN);
-
+  opcode = word << (INSTR_LEN - OPCODE_LEN);
+  opcode = opcode >> (INSTR_LEN - OPCODE_LEN);
   return opcode;
 };
 
+unsigned get_rd(unsigned word) {
+  unsigned rd;
+  rd = word >> OPCODE_LEN;
+  rd = rd & 0x1F;
+  return rd;
+}
+
 unsigned get_funct3(unsigned word) {
   unsigned funct3;
-  funct3 = word >> OPCODE_LEN + RD_LEN;
-
+  funct3 = word >> (OPCODE_LEN + RD_LEN);
+  funct3 = funct3 & 0x7;
   return funct3;
 }
 
+unsigned get_rs1(unsigned word) {
+  unsigned rs1;
+  rs1 = word >> (OPCODE_LEN + RD_LEN + FUNCT3_LEN);
+  rs1 = rs1 & 0xF;
+  return rs1;
+}
 
+unsigned get_rs2(unsigned word) {
+  unsigned rs2;
+  rs2 = word >> (OPCODE_LEN + RD_LEN + FUNCT3_LEN + RS1_LEN);
+  rs2 = rs2 & 0x1F;
+}
 
-void funct3(unsigned word) {
-
-  unsigned funct3;
-  funct3 
-  funct3 = word >> OPCODE_LEN + RD_LEN;
-  funct3 = funct3 << OPCODE_LEN + RD_LEN + 
-  //this is comment
-
-  switch 
+unsigned get_funct7(unsigned word) {
+  unsigned funct7;
+  funct7 >> (INSTR_LEN - FUNCT7_LEN);
+  return funct7;
 }
 
 
@@ -48,21 +61,28 @@ long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE 
   unsigned word;
   unsigned tmp;
   unsigned opcode;
+  unsigned funct3;
 
-  while (inst_cnt <= 5) {
+  while (inst_cnt <= 50) {
 
     // Read word (instruction set)
     word = memory_rd_w(mem, pc);
 
     // Word to byte array
     // use bitwise masking
-    tmp = (word << 25);
-    opcode = (tmp >> 25);
+    // tmp = (word << 25);
+    // opcode = (tmp >> 25);
+    opcode = get_opcode(word);
+    funct3 = get_funct3(word);
+
+
+    // Program counter
     pc += 4;
 
     printf("word: %i\n", word);
     printf("tmp: %i\n", tmp);
     printf("opcode: %u\n", opcode);
+    printf("funct3: %u\n", funct3);
     printf("pc: %0lx\n", pc);
    
     // Increase instruction count
