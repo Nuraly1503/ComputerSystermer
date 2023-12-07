ff9f8a4c55e8e7237d530be18b8ce6f29ea57ea2
kristian <mlk299@pm.me>
2023-12-07 21:14:05 +0100

    ECALL, ADDI, ORI

--- A5/src/simulate.c
+++ A5/src/simulate.c
@@ -1,16 +1,21 @@
-#include "memory.h"
-#include "assembly.h"
+#include "./memory.h"
+#include "./assembly.h"
+#include "./simulate.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Register
 #define REGISTER_LEN  32
+#define a0            10
+#define a7            17
 
 // Instruction set
 #define INSTR_LEN     32
 #define OPCODE_LEN    7
 #define RD_LEN        5
 #define FUNCT3_LEN    3
-#define RS1_LEN       4
+#define RS1_LEN       5
 #define RS2_LEN       5
 #define FUNCT7_LEN    7
 
@@ -19,6 +24,7 @@
 #define TERMINATE_93  93
 
 
+// Instruction Set helper functions
 unsigned get_opcode(unsigned word) {
   unsigned opcode;
   opcode = word << (INSTR_LEN - OPCODE_LEN);
@@ -66,18 +72,19 @@ int get_imm11(unsigned word) {
   return imm11;
 }
 
-void ecall(int reg[]) {
-  unsigned var = reg[7];
+// Ecall helper function
+void ecall(RiscvRegister_t* inst_reg) {
+  unsigned var = inst_reg->rg[a7];
   switch(var) {
     case 1:
       // returner "getchar()" i A0
       printf("getchar()\n");
-      reg[0] = getchar();
+      inst_reg->rg[a0] = getchar();
       break;
     case 2:
       // udfør "putchar(c)", hvor c tages fra A0
       printf("putchar()\n");
-      putchar(reg[0]);
+      putchar(inst_reg->rg[a0]);
       break;
     case 3:
     case 93:
@@ -88,41 +95,243 @@ void ecall(int reg[]) {
   }
 }
 
-// void type_B (unsigned word ) 
-// {
 
-//   // switch(1)// Func3 {
-//   // {
-//   //   case 
-//   // }
+/* 
+ * Immidiate Encoding Variants helper functions 
+ */
 
-// }
+// 1100011
+void type_B (unsigned word ) 
+{
+  unsigned func = get_funct3(word);
 
-void type_I_copy (unsigned long word, int reg[]) {
-  unsigned funct3 = get_funct3(word);
+  switch(func)
+  {
+    case 0:
+      printf("BEQ\n");
+      break;
+    case 1:
+      printf("BNE\n");
+      break;
+    case 4:
+      printf("BLT\n");
+      break;
+    case 5:
+      printf("BGE\n");
+      break;
+    case 6:
+      printf("BLTU\n");
+      break;
+    case 7:
+      printf("BGEU\n");
+      break;
+  };
+}
 
-  switch(funct3) {
-    case 0: ;// ADDI
-      printf("ADDI\n");
-      unsigned rs1 = get_rs1(word);
-      int imm11 = get_imm11(word);
+// 0000011
+void type_I (unsigned word ) 
+{
+  unsigned func = get_funct3(word);
+
+  switch (func)
+  {
+  case 0:
+    printf("LB\n");
+    break;
+  case 1:
+    printf("LH\n");
+    break;
+  case 2:
+    printf("LW\n");
+    break;
+  case 4:
+    printf("LBU\n");
+    break;
+  case 5:
+    printf("LHU\n");
+    break;
+  }
+}
+
+
+//0100011
+void type_S (unsigned word ) 
+{
+  unsigned func = get_funct3(word);
+
+  switch (func)
+  {
+  case 0:
+    printf("SB\n");
+    break;
+  case 1:
+    printf("SH\n");
+    break;
+  case 2:
+    printf("SW\n");
+    break;
+  }
+}
+
+//0010011
+void type_S2 (unsigned word, RiscvRegister_t* inst_reg)
+{
+  unsigned func;
+  unsigned rs1;
+  unsigned rd;
+  unsigned imm11;
+
+  func = get_funct3(word);
+
+  switch (func)
+  {
+  case 0:
+    printf("ADDI\n");
+      rs1 = get_rs1(word);
+      imm11 = get_imm11(word);
+      rd = get_rd(word);
+      printf("rs1: %u\n", rs1);
       printf("imm11: %i\n", imm11);
-      reg[rs1] = imm11;
-      printf("reg[rs1]: %i\n", reg[rs1]);
+      printf("rd: %u\n", rd);
+      inst_reg->rg[rd] = imm11 + rs1;
+      printf("reg[rs1]: %i\n", inst_reg->rg[rd]);
+    break;
+  case 2:
+    printf("SLTI\n");
+    break;
+  case 3:
+    printf("SLTIU\n");
+    break;
+  case 4:
+    printf("XORI\n");
+    break;
+  case 6:
+    printf("ORI\n");
+    rs1 = get_rs1(word);
+    imm11 = get_imm11(word);
+    rd = get_rd(word);
+    printf("rs1: %u\n", rs1);
+    printf("imm11: %i\n", imm11);
+    printf("rd: %u\n", rd);
+    int tmp = imm11 | rs1;
+    printf("tmp: %i\n", tmp);
+    inst_reg->rg[rd] = tmp;
+    printf("rg[rd]: %i\n", inst_reg->rg[rd]);
+    break;
+  case 7:
+    printf("ANDI\n");
+    break;
+  
+  // Next ones are different types with the shmat
+  
+  case 1:
+    printf("SLLI\n");
+    break;
+  case 5:
+    printf("SRLI/SRAI\n");            //OBS
+    break;
+  }
+}
+
+//0110011
+void type_R (unsigned word ) 
+{
+  unsigned func = get_funct3(word);
+
+  switch (func)
+  {
+  case 0:
+    printf("ADD/SUB\n");               /// OBS 
+    break;
+  case 1:
+    printf("SLL\n");
+    break;
+  case 2:
+    printf("SLT\n");
+    break;
+  case 3:
+    printf("SLTU\n");
+    break;
+  case 4:
+    printf("XOR\n");
+    break;
+  case 5:
+    printf("SRL/SRA\n");             /// OBS
+    break;
+  case 6:
+    printf("OR\n");
+    break;
+  case 7:
+    printf("AND\n");
+    break;
+  }
+}
+
+// Mash together type_R
+//0110011
+void helper_extension (unsigned word)
+{
+  unsigned func = get_funct3(word);
+
+  switch (func)
+  {
+    case 0:
+      printf("MUL\n");
+      break;
+    case 1:
+      printf("MULH\n");
+      break;
+    case 2:
+      printf("MULHSU\n");
+      break;
+    case 3:
+      printf("MULHU\n");
+      break;
+    case 4:
+      printf("DIV\n");
+      break;
+    case 5:
+      printf("DIVU\n");
+      break;
+    case 6:
+      printf("REM\n");
+      break;
+    case 7:
+      printf("REMU\n");
+      break;
+  }
+}
+
+
+void ex_or_nah (unsigned word)
+{
+  unsigned funct7 = get_funct7(word);
+
+  switch (funct7)
+  {
+    case 0:
+      //helper_type_R(word);
+      break;
+    case 32:
+      //helper_type_R(word);
+      break;
+    case 1:
+      //helper_extension(word);
+      break;
   }
 }
 
 
 long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file) {
 
-  int reg[REGISTER_LEN];
+  struct RiscvRegister inst_reg;
   long int pc = start_addr;  // Program counter: address of the next instruction
   long int inst_cnt = 0; // Instruction counter
   unsigned long word;
   unsigned opcode;
   unsigned funct3;
 
-  while (inst_cnt <= 50) {
+  while (inst_cnt <= 5) {
 
     // Read word (instruction set)
     word = memory_rd_w(mem, pc);
@@ -172,221 +381,21 @@ long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE 
         break;
       //type_S2
       case 19:
-        type_S2(word);
+        type_S2(word, &inst_reg);
         break;
       // If li	a7,3. LI = ADDI
       // then program is done and out c code should terminal
       // it is an ecall
       case 115:
         printf("ecall\n");
-        if (reg[7] == TERMINATE_3 || reg[7] == TERMINATE_93) {
+        if (inst_reg.rg[17] == TERMINATE_3 || inst_reg.rg[17] == TERMINATE_93) {
           // Terminate program
           return inst_cnt;
         }
-        ecall(reg);
+        ecall(&inst_reg);
         break;
     }
   }
   // finder_function(opcode);
   return inst_cnt;
-}
-
-// 1100011
-void type_B (unsigned word ) 
-{
-  unsigned func; //= get_funct3(word);
-
-
-  switch(func)// Func3 {
-  {
-    case 0:
-      printf("BEQ\n");
-      break;
-    case 1:
-      printf("BNE\n");
-      break;
-    case 4:
-      printf("BLT\n");
-      break;
-    case 5:
-      printf("BGE\n");
-      break;
-    case 6:
-      printf("BLTU\n");
-      break;
-    case 7:
-      printf("BGEU\n");
-      break;
-  };
-}
-
-// 0000011
-void type_I (unsigned word ) 
-{
-  unsigned func; //= get_funct3(word);
-
-  switch (func)
-  {
-  case 0:
-    printf("LB\n");
-    break;
-  case 1:
-    printf("LH\n");
-    break;
-  case 2:
-    printf("LW\n");
-    break;
-  case 4:
-    printf("LBU\n");
-    break;
-  case 5:
-    printf("LHU\n");
-    break;
-  }
-}
-
-
-//0100011
-void type_S (unsigned word ) 
-{
-  unsigned func; //= get_funct3(word);
-
-  switch (func)
-  {
-  case 0:
-    printf("SB\n");
-    break;
-  case 1:
-    printf("SH\n");
-    break;
-  case 2:
-    printf("SW\n");
-    break;
-  }
-}
-
-//0010011
-void type_S2 (unsigned word)
-{
-  unsigned func; // = get_funct3(word);
-
-  switch (func)
-  {
-  case 0:
-    printf("ADDI\n");
-    break;
-  case 2:
-    printf("SLTI\n");
-    break;
-  case 3:
-    printf("SLTIU\n");
-    break;
-  case 4:
-    printf("XORI\n");
-    break;
-  case 6:
-    printf("ORI\n");
-    break;
-  case 7:
-    printf("ANDI\n");
-    break;
-  
-  // Next ones are different types with the shmat
-  
-  case 1:
-    printf("SLLI\n");
-    break;
-  case 5:
-    printf("SRLI/SRAI\n");            //OBS
-    break;
-  }
-}
-
-
-//0110011
-void type_R (unsigned word ) 
-{
-  unsigned func; //= get_funct3(word);
-
-  switch (func)
-  {
-  case 0:
-    printf("ADD/SUB\n");               /// OBS 
-    break;
-  case 1:
-    printf("SLL\n");
-    break;
-  case 2:
-    printf("SLT\n");
-    break;
-  case 3:
-    printf("SLTU\n");
-    break;
-  case 4:
-    printf("XOR\n");
-    break;
-  case 5:
-    printf("SRL/SRA\n");             /// OBS
-    break;
-  case 6:
-    printf("OR\n");
-    break;
-  case 7:
-    printf("AND\n");
-    break;
-  }
-}
-
-// Mash together type_R
-//0110011
-void helper_extension (unsigned word)
-{
-  unsigned func; //= get_funct3(word);
-
-  switch (func)
-  {
-    case 0:
-      printf("MUL\n");
-      break;
-    case 1:
-      printf("MULH\n");
-      break;
-    case 2:
-      printf("MULHSU\n");
-      break;
-    case 3:
-      printf("MULHU\n");
-      break;
-    case 4:
-      printf("DIV\n");
-      break;
-    case 5:
-      printf("DIVU\n");
-      break;
-    case 6:
-      printf("REM\n");
-      break;
-    case 7:
-      printf("REMU\n");
-      break;
-  }
-}
-
-
-void ex_or_nah (unsigned word)
-{
-  unsigned funct7; //= get_funct7(word);
-
-  switch (funct7)
-  {
-    case 0:
-      helper_type_R(word);
-      break;
-    case 32:
-      helper_type_R(word);
-      break;
-    case 1:
-      helper_extension(word);
-      break;
-  }
 }
--- A5/src/simulate.h
+++ A5/src/simulate.h
@@ -5,6 +5,11 @@
 #include "assembly.h"
 #include <stdio.h>
 
+// RISC-V 32 Register
+typedef struct RiscvRegister {
+  int rg[32];
+} RiscvRegister_t;
+
 // Simuler RISC-V program i givet lager og fra given start adresse
 long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file);
 
